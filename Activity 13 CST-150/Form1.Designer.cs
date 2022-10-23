8f2471f762fb366a27cdbccc0e53783cb90dae4e
elisa <elisa@DESKTOP-NOHK2QH>
2022-10-23 11:41:35 -0700

    Add project files.

---
+++ Activity 13 CST-150/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Activity_13_CST_150
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtPlayerWins1 = new System.Windows.Forms.TextBox();
+            this.txtPlayerWins2 = new System.Windows.Forms.TextBox();
+            this.txtTies = new System.Windows.Forms.TextBox();
+            this.lblPlayerWins1 = new System.Windows.Forms.Label();
+            this.lblPlayerWins2 = new System.Windows.Forms.Label();
+            this.lblTies = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(110, 83);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 100);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(216, 83);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(100, 100);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(322, 83);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(100, 100);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(110, 189);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(100, 100);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(216, 189);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(100, 100);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(322, 189);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(100, 100);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(110, 295);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(100, 100);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(216, 295);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(100, 100);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(322, 295);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(100, 100);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(322, 401);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 55);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(110, 401);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 55);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.lblTitle.Location = new System.Drawing.Point(144, 23);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(236, 46);
+            this.lblTitle.TabIndex = 11;
+            this.lblTitle.Text = "Tic-Tac-Toe";
+            // 
+            // txtPlayerWins1
+            // 
+            this.txtPlayerWins1.Location = new System.Drawing.Point(109, 490);
+            this.txtPlayerWins1.Name = "txtPlayerWins1";
+            this.txtPlayerWins1.Size = new System.Drawing.Size(100, 22);
+            this.txtPlayerWins1.TabIndex = 12;
+            // 
+            // txtPlayerWins2
+            // 
+            this.txtPlayerWins2.Location = new System.Drawing.Point(216, 490);
+            this.txtPlayerWins2.Name = "txtPlayerWins2";
+            this.txtPlayerWins2.Size = new System.Drawing.Size(100, 22);
+            this.txtPlayerWins2.TabIndex = 13;
+            // 
+            // txtTies
+            // 
+            this.txtTies.Location = new System.Drawing.Point(323, 489);
+            this.txtTies.Name = "txtTies";
+            this.txtTies.Size = new System.Drawing.Size(100, 22);
+            this.txtTies.TabIndex = 14;
+            // 
+            // lblPlayerWins1
+            // 
+            this.lblPlayerWins1.AutoSize = true;
+            this.lblPlayerWins1.Location = new System.Drawing.Point(106, 471);
+            this.lblPlayerWins1.Name = "lblPlayerWins1";
+            this.lblPlayerWins1.Size = new System.Drawing.Size(59, 16);
+            this.lblPlayerWins1.TabIndex = 15;
+            this.lblPlayerWins1.Text = "Player 1:";
+            // 
+            // lblPlayerWins2
+            // 
+            this.lblPlayerWins2.AutoSize = true;
+            this.lblPlayerWins2.Location = new System.Drawing.Point(213, 471);
+            this.lblPlayerWins2.Name = "lblPlayerWins2";
+            this.lblPlayerWins2.Size = new System.Drawing.Size(59, 16);
+            this.lblPlayerWins2.TabIndex = 16;
+            this.lblPlayerWins2.Text = "Player 2:";
+            // 
+            // lblTies
+            // 
+            this.lblTies.AutoSize = true;
+            this.lblTies.Location = new System.Drawing.Point(320, 470);
+            this.lblTies.Name = "lblTies";
+            this.lblTies.Size = new System.Drawing.Size(37, 16);
+            this.lblTies.TabIndex = 17;
+            this.lblTies.Text = "Ties:";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(216, 401);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(100, 55);
+            this.btnRestart.TabIndex = 18;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(561, 545);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblTies);
+            this.Controls.Add(this.lblPlayerWins2);
+            this.Controls.Add(this.lblPlayerWins1);
+            this.Controls.Add(this.txtTies);
+            this.Controls.Add(this.txtPlayerWins2);
+            this.Controls.Add(this.txtPlayerWins1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Activity 13 CST-150";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtPlayerWins1;
+        private System.Windows.Forms.TextBox txtPlayerWins2;
+        private System.Windows.Forms.TextBox txtTies;
+        private System.Windows.Forms.Label lblPlayerWins1;
+        private System.Windows.Forms.Label lblPlayerWins2;
+        private System.Windows.Forms.Label lblTies;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ Activity 13 CST-150/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_13_CST_150
+{
+
+    public partial class Form1 : Form
+    {
+        //Here we declare a two dimensional array to store random values. 
+        int[,] myArray = new int[3, 3];
+
+        // Here we declare our integers. 
+        int playersWins1;
+        int playersWins2;
+        int ties;
+        int i;
+        int j;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Generate random numbers for the 2D array. 
+            Random randonNum = new Random();
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    myArray[i, j] = randonNum.Next(0, 2);
+                }
+            }
+
+            //Display an assortment of random numbers and convert them all to strings. 
+            btn1.Text = myArray[1, 1].ToString();
+            btn2.Text = myArray[0, 2].ToString();
+            btn3.Text = myArray[0, 1].ToString();
+            btn4.Text = myArray[2, 0].ToString();
+            btn5.Text = myArray[0, 0].ToString();
+            btn6.Text = myArray[2, 1].ToString();
+            btn7.Text = myArray[1, 0].ToString();
+            btn8.Text = myArray[1, 2].ToString();
+            btn9.Text = myArray[1, 1].ToString();
+
+            //Check the board to determine who won. 
+            CheckBoard();
+
+        }
+
+
+        // Clear the entire board. 
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            btn1.Text = string.Empty;
+            btn2.Text = string.Empty;
+            btn3.Text = string.Empty;
+            btn4.Text = string.Empty;
+            btn5.Text = string.Empty;
+            btn6.Text = string.Empty;
+            btn7.Text = string.Empty;
+            btn8.Text = string.Empty;
+            btn9.Text = string.Empty;
+            txtPlayerWins1.Clear();
+            txtPlayerWins2.Clear();
+            txtTies.Clear();
+            playersWins1 = 0;
+            playersWins2 = 0;
+            ties = 0;
+            i = 0;
+            j = 0;
+        }
+
+        // Exit the application 
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+        //Check The Board
+        public void CheckBoard()
+        {
+            // Set the count of marks to zero. 
+            int player1 = 0;
+            int player2 = 0;
+
+           // Check the top row, horizontally. 
+            if (btn1.Text != "" && btn2.Text != "" && btn3.Text != "")
+            {
+                if (btn1.Text == btn2.Text && btn1.Text == btn3.Text)
+                {
+                    if (btn1.Text == "1")
+                    {
+                        player1++;
+                    }
+                    else
+                    {
+                        player2++;
+                    }
+                }
+            }
+            //Check the middle row, horizontally. 
+            if (btn4.Text != "" && btn5.Text != "" && btn6.Text != "")
+            {
+                if (btn4.Text == btn5.Text && btn4.Text == btn6.Text)
+                {
+                    if (btn4.Text == "1")
+                    {
+                        player1++;
+                    }
+                    else
+                    {
+                        player2++;
+                    }
+                }
+            }
+            //Check the bottom row, horizontally. 
+            if (btn7.Text != "" && btn8.Text != "" && btn9.Text != "")
+            {
+                if (btn7.Text == btn8.Text && btn7.Text == btn9.Text)
+                {
+                    if (btn7.Text == "1")
+                    {
+                        player1++;
+                    }
+                    else
+                    {
+                        player2++;
+                    }
+                }
+            }
+            // Check the first row, starting from left to right, diagonally. 
+            if (btn1.Text != "" && btn5.Text != "" && btn9.Text != "")
+            {
+                if (btn1.Text == btn5.Text && btn1.Text == btn9.Text)
+                {
+                    if (btn1.Text == "1")
+                    {
+                        player1++;
+                    }
+                    else
+                    {
+                        player2++;
+                    }
+                }
+            }
+            // Check the second row, starting from right to left, diagonally. 
+            if (btn3.Text != "" && btn5.Text != "" && btn7.Text != "")
+            {
+                if (btn3.Text == btn5.Text && btn3.Text == btn7.Text)
+                {
+                    if (btn3.Text == "1")
+                    {
+                        player1++;
+                    }
+                    else
+                    {
+                        player2++;
+                    }
+                }
+            }
+            //Check the first row - starting from the left, vertically. 
+            if (btn1.Text != "" && btn4.Text != "" && btn7.Text != "")
+            {
+                if (btn1.Text == btn4.Text && btn1.Text == btn7.Text)
+                {
+                    if (btn1.Text == "1")
+                    {
+                        player1++;
+                    }
+                    else
+                    {
+                        player2++;
+                    }
+                }
+            }
+            //Check the  second row, vertically. 
+            if (btn2.Text != "" && btn5.Text != "" && btn8.Text != "")
+            {
+                if (btn2.Text == btn5.Text && btn2.Text == btn8.Text)
+                {
+                    if (btn2.Text == "1")
+                    {
+                        player1++;
+                    }
+                    else
+                    {
+                        player2++;
+                    }
+                }
+            }
+            //Check the last row, vertically. 
+            if (btn3.Text != "" && btn6.Text != "" && btn9.Text != "")
+            {
+                if (btn3.Text == btn6.Text && btn3.Text == btn9.Text)
+                {
+                    if (btn3.Text == "1")
+                    {
+                        player1++;
+                    }
+                    else
+                    {
+                        player2++;
+                    }
+                }
+            }
+
+
+            // Display who won. 
+            if (player1 > player2)
+            {
+                playersWins1++;
+                txtPlayerWins1.Text = playersWins1.ToString(); 
+                MessageBox.Show("Player 1 Wins The Game!");
+            }
+            if (player1 < player2)
+            {
+                playersWins2++;
+                txtPlayerWins2.Text = playersWins2.ToString();
+                MessageBox.Show("Player 2 Wins The Game!");
+            }
+            if (player1 == player2)
+            {
+                ties++;
+                txtTies.Text = ties.ToString();
+                MessageBox.Show("It's a tie!");
+            }
+        }
+    }
+}
